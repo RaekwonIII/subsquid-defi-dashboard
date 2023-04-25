00e9bdf2a73063f7ba977a254dd679fa7f5e3be8
Massimo Luraschi <massimo.lura@gmail.com>
2023-04-25 17:50:54 +0200

    Fixed % bubbles conditional formatting

--- src/components/TopCards.tsx
+++ src/components/TopCards.tsx
@@ -66,7 +66,9 @@ type DataInterface = {
 
 function TopCards() {
   const [latestData, setLatestData] = useState<DataInterface | null>(null);
-  const [yesterdayData, setYesterdayData] = useState<DataInterface | null>(null);
+  const [yesterdayData, setYesterdayData] = useState<DataInterface | null>(
+    null
+  );
   useEffect(() => {
     try {
       axios(graphQLOptions).then((response) => {
@@ -75,17 +77,32 @@ function TopCards() {
           symbol: obj.symbol,
           name: obj.name,
           totalSupply: obj.totalSupply,
-          totalSupplyStr: Number(obj.totalSupply).toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 }),
+          totalSupplyStr: Number(obj.totalSupply).toLocaleString(undefined, {
+            maximumFractionDigits: 2,
+            minimumFractionDigits: 2,
+          }),
           totalBorrows: obj.totalBorrows,
-          totalBorrowsStr: Number(obj.totalBorrows).toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 }),
+          totalBorrowsStr: Number(obj.totalBorrows).toLocaleString(undefined, {
+            maximumFractionDigits: 2,
+            minimumFractionDigits: 2,
+          }),
           supplyRate: obj.supplyRate,
           reserves: obj.reserves,
-          reservesStr: Number(obj.reserves).toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 }),
+          reservesStr: Number(obj.reserves).toLocaleString(undefined, {
+            maximumFractionDigits: 2,
+            minimumFractionDigits: 2,
+          }),
           cash: obj.cash,
-          cashStr: Number(obj.cash).toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 }),
+          cashStr: Number(obj.cash).toLocaleString(undefined, {
+            maximumFractionDigits: 2,
+            minimumFractionDigits: 2,
+          }),
           borrowRate: obj.borrowRate,
           price: obj.underlyingPriceUSD,
-          priceStr: Number(obj.underlyingPriceUSD).toLocaleString(undefined, { maximumFractionDigits: 3, minimumFractionDigits: 3 }),
+          priceStr: Number(obj.underlyingPriceUSD).toLocaleString(undefined, {
+            maximumFractionDigits: 3,
+            minimumFractionDigits: 3,
+          }),
           underlyingDecimals: obj.underlyingDecimals,
           //   date: new Date(Number(obj.blockTime)).toLocaleDateString(),
         });
@@ -129,49 +146,148 @@ function TopCards() {
         </div>
       </div>
     );
-  
-        const supplyDiff = Number((latestData.totalSupply - yesterdayData.totalSupply) * 100 / latestData.totalSupply).toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 })
-        const borrowsDiff = Number((latestData.totalBorrows - yesterdayData.totalBorrows) * 100 / latestData.totalBorrows).toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 })
-        const cashDiff = Number((latestData.cash - yesterdayData.cash) * 100 / latestData.cash).toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 })
-        const reservesDiff = Number((latestData.reserves - yesterdayData.reserves) * 100 / latestData.reserves).toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 })
-        const priceDiff = Number(Number((latestData.price - yesterdayData.price) * 100 / latestData.price).toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 }));
-        
+
+  const supplyDiff = Number(
+    Number(
+      ((latestData.totalSupply - yesterdayData.totalSupply) * 100) /
+        latestData.totalSupply
+    ).toLocaleString(undefined, {
+      maximumFractionDigits: 2,
+      minimumFractionDigits: 2,
+    })
+  );
+  console.log(`supply diff ${supplyDiff}`)
+  const borrowsDiff = Number(
+    Number(
+      ((latestData.totalBorrows - yesterdayData.totalBorrows) * 100) /
+        latestData.totalBorrows
+    ).toLocaleString(undefined, {
+      maximumFractionDigits: 2,
+      minimumFractionDigits: 2,
+    })
+  );
+  const cashDiff = Number(
+    Number(
+      ((latestData.cash - yesterdayData.cash) * 100) / latestData.cash
+    ).toLocaleString(undefined, {
+      maximumFractionDigits: 2,
+      minimumFractionDigits: 2,
+    })
+  );
+  const reservesDiff = Number(
+    Number(
+      ((latestData.reserves - yesterdayData.reserves) * 100) /
+        latestData.reserves
+    ).toLocaleString(undefined, {
+      maximumFractionDigits: 2,
+      minimumFractionDigits: 2,
+    })
+  );
+  const priceDiff = Number(
+    Number(
+      ((latestData.price - yesterdayData.price) * 100) / latestData.price
+    ).toLocaleString(undefined, {
+      maximumFractionDigits: 2,
+      minimumFractionDigits: 2,
+    })
+  );
+
   return (
     <div className="grid lg:grid-cols-9 grid-cols-5 gap-4 p-4">
       <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
         <div className="flex flex-col w-full pb-4">
-          <p className="text-2xl font-bold">{latestData.totalSupplyStr} {latestData.symbol}</p>
+          <p className="text-2xl font-bold">
+            {latestData.totalSupplyStr} {latestData.symbol}
+          </p>
           <p className="text-grey-600">Total Supply</p>
         </div>
-        <p className="bg-green-200 flex justify-center items-center p-2 rounded-lg">
-          <span className="text-green-700 text-lg">{supplyDiff}%</span>
+        <p
+          className={
+            supplyDiff < 0
+              ? "bg-red-200 flex justify-center items-center p-2 rounded-lg"
+              : "bg-green-200 flex justify-center items-center p-2 rounded-lg"
+          }
+        >
+          <span
+            className={
+              supplyDiff < 0 ? "text-red-700 text-lg" : "text-green-700 text-lg"
+            }
+          >
+            {supplyDiff}%
+          </span>
         </p>
       </div>
       <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
         <div className="flex flex-col w-full pb-4">
-          <p className="text-2xl font-bold">{latestData.totalBorrowsStr} {latestData.symbol}</p>
+          <p className="text-2xl font-bold">
+            {latestData.totalBorrowsStr} {latestData.symbol}
+          </p>
           <p className="text-grey-600">Total Borrows</p>
         </div>
-        <p className="bg-green-200 flex justify-center items-center p-2 rounded-lg">
-          <span className="text-green-700 text-lg">{borrowsDiff}%</span>
+        <p
+          className={
+            borrowsDiff < 0
+              ? "bg-red-200 flex justify-center items-center p-2 rounded-lg"
+              : "bg-green-200 flex justify-center items-center p-2 rounded-lg"
+          }
+        >
+          <span
+            className={
+              borrowsDiff < 0
+                ? "text-red-700 text-lg"
+                : "text-green-700 text-lg"
+            }
+          >
+            {borrowsDiff}%
+          </span>
         </p>
       </div>
       <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
         <div className="flex flex-col w-full pb-4">
-          <p className="text-2xl font-bold">{latestData.cashStr} {latestData.symbol}</p>
+          <p className="text-2xl font-bold">
+            {latestData.cashStr} {latestData.symbol}
+          </p>
           <p className="text-grey-600">Liquidity</p>
         </div>
-        <p className="bg-green-200 flex justify-center items-center p-2 rounded-lg">
-          <span className="text-green-700 text-lg">{cashDiff}%</span>
+        <p
+          className={
+            cashDiff < 0
+              ? "bg-red-200 flex justify-center items-center p-2 rounded-lg"
+              : "bg-green-200 flex justify-center items-center p-2 rounded-lg"
+          }
+        >
+          <span
+            className={
+              cashDiff < 0 ? "text-red-700 text-lg" : "text-green-700 text-lg"
+            }
+          >
+            {cashDiff}%
+          </span>
         </p>
       </div>
       <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
         <div className="flex flex-col w-full pb-4">
-          <p className="text-2xl font-bold">{latestData.reservesStr} {latestData.symbol}</p>
+          <p className="text-2xl font-bold">
+            {latestData.reservesStr} {latestData.symbol}
+          </p>
           <p className="text-grey-600">Reserves</p>
         </div>
-        <p className="bg-green-200 flex justify-center items-center p-2 rounded-lg">
-          <span className="text-green-700 text-lg">{reservesDiff}%</span>
+        <p
+          className={
+            reservesDiff < 0
+              ? "bg-red-200 flex justify-center items-center p-2 rounded-lg"
+              : "bg-green-200 flex justify-center items-center p-2 rounded-lg"
+          }
+        >
+          <span
+            className={
+              reservesDiff < 0
+                ? "text-red-700 text-lg"
+                : "text-green-700 text-lg"
+            }
+          >
+            {reservesDiff}%
+          </span>
         </p>
       </div>
       <div className="col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
@@ -179,16 +295,20 @@ function TopCards() {
           <p className="text-2xl font-bold">$ {latestData.priceStr}</p>
           <p className="text-grey-600">Price</p>
         </div>
-        <p className={
-                      priceDiff < 0
-                        ? "bg-red-200 flex justify-center items-center p-2 rounded-lg"
-                        : "bg-green-200 flex justify-center items-center p-2 rounded-lg"
-                    }>
-          <span className={
-                      priceDiff < 0
-                        ? "text-red-700 text-lg"
-                        : "text-green-700 text-lg"
-                    }>{priceDiff}%</span>
+        <p
+          className={
+            priceDiff < 0
+              ? "bg-red-200 flex justify-center items-center p-2 rounded-lg"
+              : "bg-green-200 flex justify-center items-center p-2 rounded-lg"
+          }
+        >
+          <span
+            className={
+              priceDiff < 0 ? "text-red-700 text-lg" : "text-green-700 text-lg"
+            }
+          >
+            {priceDiff}%
+          </span>
         </p>
       </div>
     </div>
